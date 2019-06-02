cfe475a41518b78aa88944b6333fccf429a1e783
John Monarch <31969812+johnmonarch@users.noreply.github.com>
2019-06-02 01:17:48 -0400

    Uploaded cookout.py
    Basic cookout calculator

---
+++ cookout.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+
+i = 1
+cookout_flavors = 46
+combos = 0
+total_shakes = 0
+
+while i <= cookout_flavors:
+	combos = (math.factorial(cookout_flavors))/(math.factorial(i)*(math.factorial(cookout_flavors - i)))
+	total_shakes = total_shakes + combos
+	i += 1
+
+print total_shakes
+
